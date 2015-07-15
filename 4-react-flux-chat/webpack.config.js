cca068e9fe13ecea0f5294802f0ef385a6e6ec49
Evgeniy Boyarskiy <gekabrs@gmail.com>
2015-07-15 22:11:33 +0300

    init

---
+++ 4-react-flux-chat/webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './app/index'
+  ],
+  devtool: 'eval-source-map',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  externals: {
+    "io" : "io"
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve: {
+    extensions: ['', '.js']
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loaders: ['react-hot', 'babel'],
+      include: path.join(__dirname, 'app')
+    }]
+  }
+};
